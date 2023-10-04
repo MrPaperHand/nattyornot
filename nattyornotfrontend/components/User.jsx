b0aceac23d3045f2bdd77ddbb5f7756cb9cc1838
Tom Kwon <39015477+MrPaperHand@users.noreply.github.com>
2023-10-04 15:24:43 -0700

    Delete nattyornotfrontend/components directory

--- nattyornotfrontend/components/User.jsx
+++
@@ -1,9 +0,0 @@
-  const Post = ({ user }) => (
-      <div>
-        {user.username} {post.author} 
-        <br></br>
-        <img src={post.filekey} style={imgStyles}/>
-      </div>
-    )
-    
-    export default Post
